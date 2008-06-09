dd830722b66199a3dfa8b63e5789d2761205b397
S Woodside <sbwoodside@yahoo.com>
2008-06-09 02:12:38 +0000

    still working on getting cap to work
    git-svn-id: http://shrub.ca:8080/svn/swc@47 a73b2e98-c212-4573-bb7c-f9b89b9be6a6

--- portal/config/deploy.rb
+++ portal/config/deploy.rb
@@ -1,15 +1,14 @@
+
+
 set :application, "deployed-swc-portal"
 set :repository,  "http://shrub.ca:8080/svn/swc/portal"
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
 set :deploy_to, "/disk2/home/simon/#{application}"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+set :use_sudo, false
 
-role :app, "simonwoodside.com"
-role :web, "simonwoodside.com"
-role :db,  "simonwoodside.com", :primary => true
+
+role :app, "simon@simonwoodside.com"
+role :web, "simon@simonwoodside.com"
+role :db,  "simon@simonwoodside.com", :primary => true
+
+ssh_options[:paranoid] = false 
