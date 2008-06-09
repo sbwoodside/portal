058f0686df10bb00e34babb454627993ba080dc3
S Woodside <sbwoodside@yahoo.com>
2008-06-09 04:44:44 +0000

    more cap...
    git-svn-id: http://shrub.ca:8080/svn/swc@55 a73b2e98-c212-4573-bb7c-f9b89b9be6a6

--- portal/config/deploy.rb
+++ portal/config/deploy.rb
@@ -1,6 +1,9 @@
+require 'mongrel_cluster/recipes'
+
 set :application, "deployed-swc-portal"
 set :repository,  "http://shrub.ca:8080/svn/swc/portal"
 set :deploy_to, "/disk2/home/simon/#{application}"
+set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 set :use_sudo, false
 
 role :app, "simon@simonwoodside.com"
