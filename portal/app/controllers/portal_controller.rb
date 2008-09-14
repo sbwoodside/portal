ee2028a14a790d5f663e7449905a924281d2165e
S Woodside <sbwoodside@yahoo.com>
2008-09-14 05:03:34 +0000

    Updated to read the RSS feeds from the filesystem instead of from HTTP. This is much better way.
    git-svn-id: http://shrub.ca:8080/svn/swc@64 a73b2e98-c212-4573-bb7c-f9b89b9be6a6

--- portal/app/controllers/portal_controller.rb
+++ portal/app/controllers/portal_controller.rb
@@ -8,10 +8,11 @@ class PortalController < ApplicationController
   # be very afraid of messing with the data returned by FeedNormalizer
   # and render_to_string of the RSS rxml doesn't work either
   def index
+    filesystem_dir = "#{RAILS_ROOT}/public/feed-temp"
     feed_descriptions = [
-          { :type => "flickr", :text => "Flickr image:", :url => "http://simonwoodside.com:8080/feed-temp/flickr.xml" },
-          { :type => "semacode", :text => "Semacode blog:", :url => "http://simonwoodside.com:8080/feed-temp/semacode.xml" },
-          { :type => "simonwoodside", :text => "Simon Says:", :url => "http://simonwoodside.com:8080/feed-temp/swc.xml" }
+          { :type => "flickr", :text => "Flickr image:", :file => "#{filesystem_dir}/flickr.xml" },
+          { :type => "semacode", :text => "Semacode blog:", :file => "#{filesystem_dir}/semacode.xml" },
+          { :type => "simonwoodside", :text => "Simon Says:", :file => "#{filesystem_dir}/swc.xml" }
           # Add any other feeds that you want here
           # TODO: change this to scan the public/feed-temp directory and open any file that is there
         ]
@@ -31,15 +32,15 @@ private
   # You might have the read the code to understand FeedNormalizer, sorry.
   def aggregate_feeds( feed_descriptions )
     result = feed_descriptions.map do |desc|
-      get_feed( desc[:type], desc[:text], desc[:url] )
+      get_feed( desc[:type], desc[:text], desc[:file] )
     end
     result = result.flatten
     result = result.sort_by { |x| x[:obj].date_published }.reverse
   end
-  def get_feed( type, text, url )
-    feed = FeedNormalizer::FeedNormalizer.parse open( url )
+  def get_feed( type, text, file )
+    feed = FeedNormalizer::FeedNormalizer.parse File.open( file, 'r' )
     if feed.nil?
-      logger.warn "**** Got a nil when using FeedNormalizer.parse open( #{url} )" 
+      logger.warn "**** Got a nil when using FeedNormalizer.parse File.open( #{file}, 'r' )" 
       return []
     end
     feed.entries.map do |story|
