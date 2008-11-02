f82e1549b2a0f741653a292a4c7ff5c47dbecf3d
S Woodside <sbwoodside@yahoo.com>
2008-11-02 05:56:34 +0000

    - added flickr comments  - fixed bug in title map, leaving empty titles
    git-svn-id: http://shrub.ca:8080/svn/swc@85 a73b2e98-c212-4573-bb7c-f9b89b9be6a6

--- portal/app/controllers/portal_controller.rb
+++ portal/app/controllers/portal_controller.rb
@@ -7,14 +7,17 @@ class PortalController < ApplicationController
   # main page of the portal
   # FeedTools has the advantage of handling messed up feeds
   def index
-    when_fragment_expired 'aggregate', 5.minutes.from_now do
+    @cache_ttl = 60.minutes # should use class variable but it doesn't work
+    when_fragment_expired 'aggregate', @cache_ttl.from_now do
       # This is the array of feeds you want to aggregate
       uris = ['http://simonwoodside.com:8080/posts/rss', 'http://simonwoodside.com/comments/rss',
               'http://semacode.com/posts/rss',
-              'http://api.flickr.com/services/feeds/photos_public.gne?id=20938094@N00&lang=en-us&format=rss_200']
+              'http://api.flickr.com/services/feeds/photos_public.gne?id=20938094@N00&lang=en-us&format=rss_200',
+              'http://api.flickr.com/services/feeds/activity.gne?user_id=20938094@N00']
       # Here you should make a map between the "official" feed title in the XML, and what you want to show on the portal
-      title_map = { "Simon Says" => "Simon Says:", "Simon Says: Comments" => "Simon Says Comment:",
-                    "Uploads from sbwoodside" => "Flickr Picture:", "Semacode" => "Semacode Blog:"}
+      title_map = { "Simon Says" => "Simon Says post:", "Simon Says: Comments" => "Simon Says comment:",
+                    "Uploads from sbwoodside" => "Flickr picture:", "Semacode" => "Semacode blog post:",
+                    'Comments on your photostream and/or sets' => 'Flickr comment:' }
       @all = aggregate_feeds uris, title_map
     end
   end
@@ -29,7 +32,7 @@ private
   # Where :title is a string for the UI, and :feed_item is a FeedTools:FeedItem object
   def aggregate_feeds( uris, title_map )
     all_feed_items = uris.map { |uri| get_feed( uri ) } .flatten # get all of the feed data
-    all_feed_items.each { |item| item[:title] = title_map[item[:title]] } # map the feed's title to our favored title
+    all_feed_items.each { |item| title_map[item[:title]] && item[:title] = title_map[item[:title]] } # map the feed's title to our favored title
     all_feed_items = all_feed_items.sort_by { |x| x[:feed_item].published }.reverse # sort by date published
   end
   
