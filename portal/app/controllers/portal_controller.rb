ac89bfec582fcd50a15e9a5af014d9ba81847f3f
S Woodside <sbwoodside@yahoo.com>
2008-12-07 02:32:32 +0000

    Working on a way to have recaching be asynchronous. Also added a secret.
    git-svn-id: http://shrub.ca:8080/svn/swc@89 a73b2e98-c212-4573-bb7c-f9b89b9be6a6

--- portal/app/controllers/portal_controller.rb
+++ portal/app/controllers/portal_controller.rb
@@ -7,6 +7,7 @@ class PortalController < ApplicationController
   # read from a (database/rails) cache. Also cache the PARSED data, not the xml data.
   # TODO add XML feed output
   
+  @@secret = "8_2BGh0" # change this to protect your site from DoS attack
   # This is the array of feeds you want to aggregate.
   # Currently if you change this you need to manually delete all the records in the cache
   @@uris = ['http://simonwoodside.com:8080/posts/rss', 'http://simonwoodside.com/comments/rss',
@@ -19,8 +20,10 @@ class PortalController < ApplicationController
                   'Comments on your photostream and/or sets' => 'Flickr comment:' }
   
   def index # When you want to recache the feeds, call /?recache=yes. do that regularly from cron.
-    if params[:recache]  # while this is running, the existing cache will still be used
-      @@uris.each { |uri| cache_feed uri }
+    if params[:recache] and @@secret == params[:secret]
+      logger.warn "SIMON -------- ABOUT TO START RECACHE"
+      @@uris.each { |uri| cache_feed uri } # while this is running, the existing cache should still be used
+      logger.warn "SIMON -------- DONE RECACHE ABOUT TO EXPIRE FRAGMENT"
       expire_fragment(:controller => 'portal', :action => 'index')
     end
     unless read_fragment({})
@@ -30,8 +33,25 @@ class PortalController < ApplicationController
       @all = @all.sort_by { |x| x[:feed_item].published }.reverse # sort by date published
     end
   end
+  #def index # When you want to recache the feeds, call /?recache=yes. do that regularly from cron.
+  #  if params[:recache]
+  #    @@uris.each { |uri| cache_feed uri }
+  #    @all = read_cache
+  #    expire_fragment(:controller => 'portal', :action => 'index')
+  #  else
+  #    unless read_fragment({})
+  #      @all = read_cache
+  #    end
+  #  end
+  #end
   
 private
+  # Make an array of hashes, each hash is { :title, :feed_item (FeedTools:FeedItem object) }
+  def read_cache
+    all = @@uris.map { |uri| get_feed( uri ) } .flatten # get all of the feed data
+    all.each { |item| @@title_map[item[:title]] && item[:title] = @@title_map[item[:title]] } # map the feed's title to our favored title
+    return all.sort_by { |x| x[:feed_item].published }.reverse # sort by date published
+  end
   # This will replace cached feeds in the DB that have the same URI
   def cache_feed( uri )
     puts "cache_feed( #{uri} )" # this can be VERY slow
@@ -39,7 +59,6 @@ private
     new.parsed_feed = FeedTools::Feed.open( uri )
     new.save!
   end
-  
   def get_feed( uri )
     parsed_feed = CachedFeed.find_by_uri( uri ).parsed_feed
     parsed_feed.items.map { |feed_item| { :title => parsed_feed.title, :feed_item => feed_item } } # implicit return value
