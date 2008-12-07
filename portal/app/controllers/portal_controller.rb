4e7202a5492aa421bab3fa9884fd654a6aa54bae
S Woodside <sbwoodside@yahoo.com>
2008-12-07 04:41:21 +0000

    Well, this is pretty good I guess. Tie up the database minimally. I'm still not sure why it seems to prevent other threads from accessing. Maybe I'm imagining it. Also added a testing mongrel cluster config and tidied up the code.
    git-svn-id: http://shrub.ca:8080/svn/swc@92 a73b2e98-c212-4573-bb7c-f9b89b9be6a6

--- portal/app/controllers/portal_controller.rb
+++ portal/app/controllers/portal_controller.rb
@@ -3,71 +3,47 @@ require 'timed_fragment_cache'
 
 class PortalController < ApplicationController
   layout 'site'
-  # We use FeedTools because it can handle messed up feeds. BUT, it's REALLY slow, so we should almost always
-  # read from a (database/rails) cache. Also cache the PARSED data, not the xml data.
+  # Instructions: 1. Change @@secret. 2. Add a cron job to regularly call /?recache=yes&secret=XXXXXXX
+  # This is a feed aggregator that uses FeedTools because it handles practically any feed.
+  # But FeedTools is super slow so this aggregator caches the _parsed_ feeds in the database.
   # TODO add XML feed output
   
   @@secret = "8_2BGh0" # change this to protect your site from DoS attack
-  # This is the array of feeds you want to aggregate.
-  # Currently if you change this you need to manually delete all the records in the cache
+  # The array of feeds you want to aggregate. If you change this then manually delete the whole cache.
   @@uris = ['http://simonwoodside.com:8080/posts/rss', 'http://simonwoodside.com/comments/rss',
             'http://semacode.com/posts/rss',
             'http://api.flickr.com/services/feeds/photos_public.gne?id=20938094@N00&lang=en-us&format=rss_200',
             'http://api.flickr.com/services/feeds/activity.gne?user_id=20938094@N00']
-  # Here you should make a map between the "official" feed title in the XML, and what you want to show on the portal
+  # A map between the "official" feed titles in the XML, and the titles you want to show when rendered.
   @@title_map = { "Simon Says" => "Simon Says:", "Simon Says: Comments" => "Simon Says comment:",
                   "Uploads from sbwoodside" => "Flickr picture:", "Semacode" => "Semacode blog post:",
                   'Comments on your photostream and/or sets' => 'Flickr comment:' }
   
-  #def index # When you want to recache the feeds, call /?recache=yes. do that regularly from cron.
-  #  if params[:recache] and @@secret == params[:secret]
-  #    puts "SIMON -------- ABOUT TO START RECACHE"
-  #    @@uris.each { |uri| cache_feed uri } # while this is running, the existing cache should still be used
-  #    puts "SIMON -------- DONE RECACHE ABOUT TO EXPIRE FRAGMENT"
-  #    expire_fragment(:controller => 'portal', :action => 'index')
-  #  end
-  #  unless read_fragment({})
-  #    # Make an array of hashes, each hash is { :title, :feed_item (FeedTools:FeedItem object) }
-  #    @all = @@uris.map { |uri| get_feed( uri ) } .flatten # get all of the feed data
-  #    @all.each { |item| @@title_map[item[:title]] && item[:title] = @@title_map[item[:title]] } # map the feed's title to our favored title
-  #    @all = @all.sort_by { |x| x[:feed_item].published }.reverse # sort by date published
-  #  end
-  #end
-  def index # When you want to recache the feeds, call /?recache=yes. do that regularly from cron.
-    if params[:recache]
-      puts "SIMON -------- ABOUT TO START RECACHE"
-      @@uris.each { |uri| cache_feed uri }
-      puts "SIMON -------- DONE RECACHE ABOUT TO READ CACHE"
-      @all = read_cache
-      puts "SIMON -------- DONE READ CACHE ABOUT TO EXPIRE FRAGMENT"
-      expire_fragment(:controller => 'portal', :action => 'index')
-      puts "SIMON -------- DONE EXPIRE FRAGMENT"
+  def index
+    if params[:recache] and @@secret == params[:secret]
+      cache_feeds
+      expire_fragment(:controller => 'portal', :action => 'index') # next load of index will re-fragment cache
+      render :text => "Done recaching feeds"
     else
-      unless read_fragment({})
-        @all = read_cache
-      end
+      @all = read_cache unless read_fragment({})
     end
   end
   
 private
+  # This will replace cached feeds in the DB that have the same URI. Be careful not to tie up the DB connection.
+  def cache_feeds
+    puts "caching feeds... (can be slow)"
+    results = @@uris.map { |uri| {:uri => uri, :parsed_feed => FeedTools::Feed.open( uri )} }
+    results.each { |result| CachedFeed.find_or_create_by_uri( result[:uri], :parsed_feed => result[:parsed_feed] ) } # not sure if this does an update...
+  end
   # Make an array of hashes, each hash is { :title, :feed_item (FeedTools:FeedItem object) }
   def read_cache
     all = @@uris.map { |uri| get_feed( uri ) } .flatten # get all of the feed data
     all.each { |item| @@title_map[item[:title]] && item[:title] = @@title_map[item[:title]] } # map the feed's title to our favored title
     return all.sort_by { |x| x[:feed_item].published }.reverse # sort by date published
   end
-  # This will replace cached feeds in the DB that have the same URI
-  def cache_feed( uri )
-    puts "cache_feed( #{uri} )" # this can be VERY slow
-    new = CachedFeed.find_or_initialize_by_uri( uri )
-    new.parsed_feed = FeedTools::Feed.open( uri )
-    new.save!
-  end
   def get_feed( uri )
     parsed_feed = CachedFeed.find_by_uri( uri ).parsed_feed
     parsed_feed.items.map { |feed_item| { :title => parsed_feed.title, :feed_item => feed_item } } # implicit return value
-  rescue
-    puts "**** Error: get_feed threw an exception (#{$!}), but I'm failing gracefully." and return []
   end
-  
 end
