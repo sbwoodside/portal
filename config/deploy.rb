bed3570b169887d0cd38326b06c5ec255c1caf53
S Woodside <sbwoodside@yahoo.com>
2009-04-02 23:27:57 -0400

    Configuration changes for new server.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,16 @@
+# This is for capistrano
+
 require 'mongrel_cluster/recipes'
 
 set :application, "deployed-swc-portal"
-set :repository,  "http://shrub.ca:8080/svn/swc/portal"
-set :deploy_to, "/disk2/home/simon/#{application}"
+set :repository,  "git://github.com/sbwoodside/portal"
+set :scm, "git"
+set :deploy_to, "/home/simon/#{application}"
 set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 set :use_sudo, false
 
-role :app, "simon@simonwoodside.com"
-role :web, "simon@simonwoodside.com"
-role :db,  "simon@simonwoodside.com", :primary => true
+role :app, "simon@new.shrub.ca"
+role :web, "simon@new.shrub.ca"
+role :db,  "simon@new.shrub.ca", :primary => true
 
 ssh_options[:paranoid] = false 
