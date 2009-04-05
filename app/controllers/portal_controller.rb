7523479d9a3f5880b719925c007896faeacccee0
S Woodside <sbwoodside@yahoo.com>
2009-04-04 21:17:16 -0400

    Improved configuration using eugenebolshakov/app_config with YAML, improved README documentation.

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -1,25 +1,14 @@
 require 'feed_tools'
 
+# This is a feed aggregator that uses FeedTools because it handles practically any feed.
+# But FeedTools is super slow in every way so this aggregator stops using it as soon as possible.
+# TODO add XML feed output
 class PortalController < ApplicationController
   layout 'site'
-  # Instructions: 1. Change @@secret. 2. Add a cron job to regularly call /?recache=yes&secret=XXXXXXX
-  # This is a feed aggregator that uses FeedTools because it handles practically any feed.
-  # But FeedTools is super slow in every way so this aggregator stops using it as soon as possible.
-  # TODO add XML feed output
-  
-  @@secret = "8_2BGh0" # change this to protect your site from DoS attack
-  # The array of feeds you want to aggregate. If you change this then manually delete the whole cache.
-  @@uris = ['http://simonwoodside.com/posts/rss', 'http://simonwoodside.com/comments/rss',
-            'http://semacode.com/posts/rss',
-            'http://api.flickr.com/services/feeds/photos_public.gne?id=20938094@N00&lang=en-us&format=rss_200',
-            'http://api.flickr.com/services/feeds/activity.gne?user_id=20938094@N00']
-  # A map between the "official" feed titles in the XML, and the titles you want to show when rendered.
-  @@title_map = { "Simon Says" => "Simon Says:", "Simon Says: Comments" => "Simon Says comment:",
-                  "Uploads from sbwoodside" => "Flickr picture:", "Semacode" => "Semacode blog post:",
-                  'Comments on your photostream and/or sets' => 'Flickr comment:' }
   
   def index
-    if params[:recache] and @@secret == params[:secret]
+    puts Conf.title_map.inspect
+    if params[:recache] and Conf.secret == params[:secret]
       cache_feeds
       expire_fragment(:controller => 'portal', :action => 'index') # next load of index will re-fragment cache
       render :text => "Done recaching feeds"
@@ -32,7 +21,7 @@ private
   # This will replace cached feeds in the DB that have the same URI. Be careful not to tie up the DB connection.
   def cache_feeds
     puts "Caching feeds... (can be slow)"
-    feeds = @@uris.map do |uri|
+    feeds = Conf.feeds.map do |uri|
       feed = FeedTools::Feed.open( uri )
       { :uri => uri, :title => feed.title, 
         :items => feed.items.map { |item| {:title => item.title, :published => item.published, :link => item.link} } }
@@ -45,9 +34,9 @@ private
   end
   # Make an array of hashes, each hash is { :title, :feed_item }
   def read_cache
-    @@uris.map { |uri|
+    Conf.feeds.map { |uri|
       feed = CachedFeed.find_by_uri( uri ).parsed_feed
-      feed[:items].map { |item| {:feed_title => @@title_map[feed[:title]] || feed[:title], :feed_item => item} }
+      feed[:items].map { |item| {:feed_title => Conf.title_map[feed[:title]] || feed[:title], :feed_item => item} }
     } .flatten .sort_by { |item| item[:feed_item][:published] } .reverse
   end
 end
